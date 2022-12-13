bafc47c39ea7071c52aa892b89b3baadff2c40fc
Arvin De Los Santos <arvin.delossantos@cognizant.com>
2022-12-13 21:57:48 +0800

    Created Breadcrumbs component

--- src/app/layouts/Breadcrumbs/index.tsx
+++ src/app/layouts/Breadcrumbs/index.tsx
@@ -1,15 +1,21 @@
 import { useContext } from "react"
 import { BreadcrumbContext } from "../../context/BreadcrumbContext"
+import styles from "./breadcrumbs.module.scss";
 
 export default function Breadcrumbs(){
     const breadcrumbContext = useContext(BreadcrumbContext);
     return (
-        <div className="breadcrumbs">
-            <ul>
+        <div className={styles.breadcrumbs_container}>
+            <ul className={styles.breadcrumbs}>
                 {
-                    breadcrumbContext?.breadcrumbState.map(crumb => {
+                    breadcrumbContext?.breadcrumbState.map((crumb, index: number) => {
                         return (
-                            <li key={crumb.path}>{`${crumb.label} ${crumb.path}`}</li>
+                            <li key={crumb.path}>
+                                {index !== 0 && (
+                                    <span>/</span>
+                                )} 
+                                {`${crumb.label}`}
+                            </li>
                         )
                     })
                 }
