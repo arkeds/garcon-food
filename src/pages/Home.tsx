8a3d6bbe4bb0e7059220bd0b4a69c19014c6f6b7
Arvin De Los Santos <arvin.delossantos@cognizant.com>
2022-12-13 21:57:22 +0800

    Update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
 import { useContext, useEffect } from "react";
 import { BreadcrumbContext } from "../app/context/BreadcrumbContext";
+import Tile from "../app/features/Tile";
+import styles from './home.module.scss';
+import MenuIcon from './../assets/icons/menu.svg';
+import OrderIcon from './../assets/icons/order.svg';
 
 export default function Home() {
 
   const breadcrumbContext = useContext(BreadcrumbContext)
-  
-  useEffect(() => {
-    breadcrumbContext?.setCrumbs([{path: '/home', label: 'Home'}])
 
-    return () => {breadcrumbContext?.setCrumbs([])}
+  useEffect(() => {
+    breadcrumbContext?.setCrumbs([{ path: '/home', label: 'Home' }])
+
+    return () => { breadcrumbContext?.setCrumbs([]) }
   }, [])
-  return <h1>Home</h1>;
+  return (
+    <main className={styles.page_container}>
+      <div className={styles.menu_container}>
+        <Tile imageUrl={OrderIcon} linkTo="/order-status" tileLabel="Order Status" />
+        <Tile imageUrl={MenuIcon} linkTo="/menu" tileLabel="Menu" />
+      </div>
+    </main>
+  );
 }
